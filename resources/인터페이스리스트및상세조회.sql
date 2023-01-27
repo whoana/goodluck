0da92d4eb25208490a789a1dd98ee8ae9f08241d
whoana <whoana@gmail.com>
2023-01-27 15:17:20 +0900

    교육샘플

--- resources/인터페이스리스트및상세조회.sql
+++ resources/인터페이스리스트및상세조회.sql
@@ -1,20 +1,22 @@
+-------------------------
+-- 인터페이스 리스트 조회
+------------------------- 
 SELECT 	
-		 a.INTEGRATION_ID
-		,a.INTERFACE_NM
-		,b.STATUS 
-		,b.BUSINESS_ID 
+		 a.INTEGRATION_ID	AS "인터페이스ID"
+		,a.INTERFACE_NM		AS "인터페이스명"
+--		,b.STATUS 
+		,''					AS "상태명"
 		,b01.BUSINESS_NM	AS "업무명"
-		,c.SYSTEM_ID 
 		,c01.SYSTEM_NM  	AS "송신시스템명"
-		,d.SYSTEM_ID 
 		,d01.SYSTEM_NM  	AS "수신시스템명"
-		,a.DATA_PR_DIR
-		,a.APP_PR_METHOD
-		,a.DATA_PR_DIR
-		,a.REG_USER
+--		,a.DATA_PR_DIR
+		,''					AS "데이터처리방향"
+--		,a.APP_PR_METHOD
+		,'' 				AS "APP처리방식"
+--		,a.DATA_PR_DIR
+		,''					AS "데이터처리방향"
 		,a01.USER_NM 		AS "등록자명"
 		,a.REG_DATE
-		,a.MOD_USER
 		,a02.USER_NM 		AS "수정자명"
 		,a.MOD_DATE
   	  FROM TAN0201 a
@@ -38,3 +40,26 @@ INNER JOIN tsu0101 a01
 		ON a01.USER_ID = a.REG_USER 
 LEFT OUTER JOIN tsu0101 a02 
 		ON a02.USER_ID = a.MOD_USER
+		
+-------------------------
+-- 인터페이스 상세 조회
+-------------------------
+SELECT 
+	 인터페이스ID(인티그레이션ID)
+	,인터페이스명 
+	,상태명 
+	,업무명  
+	,송신시스템명 
+	,수신시스템명 
+	,데이터처리방식명  
+	,APP처리방식
+	,데이터처리방향명 		
+	,등록일 (yyyymmdd hh24miss)
+	,등록자명 
+	,수정일 
+	,수정자명 
+	,송신담당자명 
+	,수신담당자명  
+  FROM tan0201
+ WHERE INTERFACE_ID = 'F@00000508'
+;
