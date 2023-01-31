6912df8f37843e2ccc0b3e17913406743cc09dcd
whoana <whoana@gmail.com>
2023-01-31 15:01:45 +0900

    office

--- resources/GROUPBY.sql
+++ resources/GROUPBY.sql
@@ -1,9 +1,9 @@
--- 2023년 이후로 등록된 일자별 인터페이스 건수를 구해볼까나?
+------------------------------------------
+-- 2023년 이후로 등록된 일자별 인터페이스 건수
+------------------------------------------
 SELECT 
-	 substr(a.REG_DATE,1, 8)
-	,count(a.INTERFACE_ID)
-	,max(a.REG_USER)
-	,a.MOD_USER
+	 substr(a.REG_DATE,1, 8)    as "date"
+	,count(a.INTERFACE_ID)      as "cnt"
   FROM tan0201 a
  WHERE a.REG_DATE >= '20220101' || '000000000'
 GROUP BY substr(a.REG_DATE,1, 8) , a.MOD_USER
