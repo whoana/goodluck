f6d664ee06fbf916d8f5f56bf8fcbb268e50ea45
kkl0712 <kkl0712@mocomsys.com>
2023-02-03 10:38:09 +0900

    kkl

---
+++ resources/java/gyulee/test03.java
@@ -0,0 +1,5 @@
+public class test03{
+	public static void main(String[] args){
+		System.out.println("2023.02.03");
+	}
+}
---
+++ resources/java/gyulee/test04.java
@@ -0,0 +1,8 @@
+public class test04{
+	/* 한국어로
+	인코딩하기 */
+
+	public static void main(String[] args){
+		System.out.println("한국어로 띄워볼까요");	
+	}
+}
