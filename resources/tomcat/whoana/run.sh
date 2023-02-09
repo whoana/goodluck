516884e513ec7fe845509a315b74ac4a72a77727
whoana <whoana@gmail.com>
2023-02-09 16:16:45 +0900

    add tomcat resources

---
+++ resources/tomcat/whoana/run.sh
@@ -0,0 +1,9 @@
+# echo "CATALINA_HOME: %CATALINA_HOME%"
+# se CATALINA_BASE=/Users/whoana/DEV/education/goodluck/resources/tomcat/whoana
+# catalina.bat configtest 
+# catalina.bat run
+
+echo "CATALINA_HOME: $CATALINA_HOME"
+export CATALINA_BASE=/Users/whoana/DEV/education/goodluck/resources/tomcat/whoana
+#catalina.sh configtest 
+catalina.sh run
